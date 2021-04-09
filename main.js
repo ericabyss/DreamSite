f88cfb1a088bdf5af726f5420d91b9fd0b61ac6d
ericabyss <erica051746@naver.com>
2021-04-09 17:51:14 +0900

    js trans

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 50 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
-});
+'use strict'
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(`navbarHeight: ${navbarHeight}`);
+})
